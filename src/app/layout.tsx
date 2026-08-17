5184ebcd5024079e285dd8938b11a085b7338688
Anthony Lewis <ignatiuspennyfeatherix@users.noreply.github.com>
2026-08-17 16:51:34 +0100

    Add navigation for game modes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><div className="siteLinks"><a href="/">Predictions</a><a href="/preseason">Pre-season</a><a href="/admin">Commissioner</a></div>{children}</body>
     </html>
   );
 }
